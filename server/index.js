7206528eb51aa095d2879cab7d4c70231ac4c842
Minh <minhvn29@gmail.com>
2020-05-04 22:45:34 -0700

    Render all 4 services on proxy

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/../client/public'));
+app.use(express.static(__dirname + '../public'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -16,6 +16,11 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
+});
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../public/index.html'));
+
 });
 
 
--- server/start.js
+++ server/start.js
@@ -1,5 +1,5 @@
 const app = require('./index.js');
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
